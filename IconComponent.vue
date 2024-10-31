bd0482629e97ce34a47eaf0031600cdfb5a54496
Will980410 <131024524+Will980410@users.noreply.github.com>
2024-10-31 12:31:54 -0500

    Add files via upload

---
+++ IconComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <ion-icon :name="iconName" :color="color" :size="size"></ion-icon>
+  </template>
+  
+  <script>
+  export default {
+    name: 'IconComponent',
+    props: {
+      iconName: { type: String, default: 'star' },
+      color: { type: String, default: 'primary' },
+      size: { type: String, default: 'large' }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  ion-icon {
+    margin: 5px;
+  }
+  </style>
+  
